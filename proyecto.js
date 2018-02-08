124c3981dccaeacb4e0c389902f7ed70347419cd
Isabel Garcia <isabel22@galileo.edu>
2018-02-08 18:37:15 +0100

    cambiando formato en nombre de variables

--- proyecto.js
+++ proyecto.js
@@ -1,16 +1,16 @@
-var nombre_completo = prompt("Por favor ingrese el nombre completo:", "Nombres y apellidos");
-var dia_nacimiento = prompt("Ingrese el día de nacimiento", "Ejemplo: Si nació el 22 de enero, solamente ingresar 22");
-var mes_nacimiento = prompt("Ingrese el mes de nacimiento", "Ejemplo: Si nació en enero, solamente ingresar 1 porque es el mes 1");
-var anio_nacimiento = prompt("Ingrese el año de nacimiento", "Ejemplo: Si nació el 22 de enero de 1987, solamente ingresar 1987");
+var nombreCompleto = prompt("Por favor ingrese el nombre completo:", "Nombres y apellidos");
+var diaNacimiento = prompt("Ingrese el día de nacimiento", "Ejemplo: Si nació el 22 de enero, solamente ingresar 22");
+var mesNacimiento = prompt("Ingrese el mes de nacimiento", "Ejemplo: Si nació en enero, solamente ingresar 1 porque es el mes 1");
+var anioNacimiento = prompt("Ingrese el año de nacimiento", "Ejemplo: Si nació el 22 de enero de 1987, solamente ingresar 1987");
 
 var conyuge = prompt("¿Tiene cónyuge?", "SI/NO");
 var hijos = prompt("¿Tiene hijos?", "SI/NO");
-var cantidad_hijos = prompt("Ingrese la cantidad de hijos menores de 21 años:", "Por favor ingrese únicamente números");
+var cantidadHijos = prompt("Ingrese la cantidad de hijos menores de 21 años:", "Por favor ingrese únicamente números");
 
-const precio_base = 250;
+const precioBase = 250;
 
 
-comision = precio_base * 0.30;
+comision = precioBase * 0.30;
 
 // El siguiente es un comentario.
 // Por favor no elimine los caracteres // que se encuentran al inicio.
@@ -21,5 +21,5 @@ comision = precio_base * 0.30;
 recargos = 0;
 
 
-total_a_pagar = precio_base + comision + recargos;
-document.write(total_a_pagar);
+totalPagar = precioBase + comision + recargos;
+document.write(totalPagar);
